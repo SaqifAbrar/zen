c8c13db2b67b6f6ca5a1eaba36b24bd9d24db386
shaiza <shaiza@uoguelph.ca>
2024-02-17 11:20:10 -0500

    Base setup complete, creating components now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import Search from "./components/search"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>Heading</h1>
+        <h2>Subheading</h2>
+        <h3>Subtitle</h3>
+        <p>Paragraph Text</p>
+      </div>      
     </div>
   );
 }
---
+++ frontend/src/components/search.jsx
@@ -0,0 +1,13 @@
+import "../styles/search.css"
+import Logo from "../assets/koiZen.svg"
+
+function Search(){
+    return (
+        <div className="search">
+            <h1>testing</h1>
+        </div>
+
+    );
+}
+
+export default Search;
