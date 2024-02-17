67ae86e9106e7bfa7b44bf5208890a9b148142d8
Saqif Abrar <abrars@uoguelph.ca>
2024-02-17 14:40:19 -0500

    Added more dependancies
    Will probably remove them, this is for the sake of testing

--- backend/llm/indexer.py
+++ backend/llm/indexer.py
@@ -1,1 +1,15 @@
-from llama_index.llms import Ollama
+from llama_index.llms.ollama import Ollama
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+
+#llm = Ollama(model="llama2")
+
+#resp = llm.complete("Who is Paul Graham?")
+
+#print(resp)
+service_context = ServiceContext.from_defaults(chunk_size=1024, llm="local", embed_model="local")
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+response = query_engine.query("What is a Koi fish?")
+
+print(response)
