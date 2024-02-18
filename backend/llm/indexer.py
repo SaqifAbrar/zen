f10f343c7192e10b85f6954daf651b5e630f5762
Saqif Abrar <abrars@uoguelph.ca>
2024-02-17 20:35:39 -0500

    Updated Zen RAG for URL contexts and reduced venv dependancies

--- backend/llm/indexer.py
+++ backend/llm/indexer.py
@@ -1,15 +1,27 @@
-from llama_index.llms.ollama import Ollama
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+from langchain_community.llms import Ollama
+from langchain_community.prompts import PromptTemplate
+from langchain_community.chain import LLMChain
 
-#llm = Ollama(model="llama2")
+llm = Ollama(model="llama2")
+print(llm.invoke("the first man on the moon was..."))
 
-#resp = llm.complete("Who is Paul Graham?")
+what_prompt = PromptTemplate(
+  input_variables=["topic"],
+  template="Give me 5 interesting facts about {topic}."
+)
 
-#print(resp)
-service_context = ServiceContext.from_defaults(chunk_size=1024, llm="local", embed_model="local")
+
+
+'''import os
+os.environ["OPENAI_API_KEY"] = "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"
+
+from llama_index.llms.openai import OpenAI
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+# from IPython.display import Markdown, display
+
 documents = SimpleDirectoryReader("data").load_data()
-index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine()
-response = query_engine.query("What is a Koi fish?")
 
-print(response)
+index = VectorStoreIndex.from_documents(documents)
+
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
+index = load_index_from_storage(storage_context=storage_context)''' 
