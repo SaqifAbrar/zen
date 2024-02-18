5fe0ae670ec31f31c29aca18096b735cc958ebb5
Saqif Abrar <abrars@uoguelph.ca>
2024-02-18 08:15:34 -0500

    Updated browser router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,28 @@
-import Search from "./components/search"
-import MyForm from "./components/results"
+import Search from "./components/search";
+import MyForm from "./components/results";
 
-import './App.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <BrowserRouter>
-          {/* <Navbar/> */}
-          <Routes>
-            <Route path="query" element={<MyForm />} />
-            <Route path="/" element={<Search />}>
-            </Route>
-          </Routes>
-          {/* <Navbar/> */}
-        </BrowserRouter>
-
-
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="App-header">
+				<BrowserRouter basename={process.env.PUBLIC_URL}>
+					{/* <Navbar/> */}
+					<Routes>
+						<Route path="query" element={<MyForm />} />
+						<Route path="/" element={<Search />}></Route>
+					</Routes>
+					{/* <Navbar/> */}
+				</BrowserRouter>
+			</div>
+		</div>
+	);
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(<App />);
 export default App;
